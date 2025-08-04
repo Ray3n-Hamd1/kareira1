057acdc93ced75abe4eb48893219dfc1bcb32e10
rayen hamdi <154424440+Ray3n-Hamd1@users.noreply.github.com>
2025-08-04 16:53:51 +0100

    ninja

--- kariera_front/frontend/src/App.js
+++ kariera_front/frontend/src/App.js
@@ -16,7 +16,6 @@ import ProjectDetails from "./ProjectDetails";
 import JobSearchDashboard from "./JobSearchDashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import PersonalInfoPage from "./components/resume-builder/PersonalInfoPage";
 import WorkHistoryPage from "./components/resume-builder/WorkHistoryPage";
 import JobDescriptionPage from "./components/resume-builder/JobDescriptionPage";
 import WorkHistorySummaryPage from "./components/resume-builder/WorkHistorySummaryPage";
@@ -62,7 +61,7 @@ function App() {
                 <PrivateRoute>
                   <>
                     <Navbar />
-                    <PersonalInfoPage />
+                    <ResumeBuilderPage1 />
                   </>
                 </PrivateRoute>
               }
